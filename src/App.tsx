c63467b6c19ad67c3973716d6b5eee89ac43425d
username91355 <personalmailD13@gmail.com>
2022-11-27 16:10:26 +0300

    debounce fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
+import useDebounce from "./hooks/useDebounce";
+import useThrottle from './hooks/useThrottle';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [sort, setSort] = useState('');
+	const debValue = useDebounce(sort);
+	const [value, setValue] = useState('hello')
+	const throttledValue = useThrottle(value)
+
+	useEffect(() => console.log(`throttledValue changed: ${throttledValue}`), [
+		throttledValue,
+	])
+	const makeRequest = (value: string) => {
+		console.log('Request: ', value);
+	}
+
+	useEffect(() => {
+		makeRequest(debValue)
+	}, [debValue]);
+
+	const changeSort = (e: ChangeEvent<HTMLInputElement>) => {
+		setSort(e.target.value);
+	}
+
+	function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+		setValue(event.target.value)
+	}
+
+	return (
+		<div className="App">
+			<div>
+				Debounce: <input type="text" value={sort} onChange={changeSort}/>
+			</div>
+			<div>
+				Throttle: <input value={value} onChange={onChange}/>
+				<p>Throttled value: {throttledValue}</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
