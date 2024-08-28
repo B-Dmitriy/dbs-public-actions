07c853920f4a62a413a93b31c56d6f521018e063
B-Dmitriy <balyaevds.main@gmail.com>
2024-08-28 10:53:44 +0300

    init: lib

---
+++ dist/index.es.js
@@ -0,0 +1,17 @@
+class t {
+  constructor(n) {
+    this.name = n;
+  }
+  sayName() {
+    console.log(this.name);
+  }
+}
+const o = 3.14;
+function e(s) {
+  return o + s;
+}
+export {
+  o as PI,
+  t as User,
+  e as addIP
+};
